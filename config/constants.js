6dfe9567a678f76f09aad6e1a41d8dced6c59711
Igor Korovchenko <igsekor@gmail.com>
2025-01-13 03:19:59 +0300

    Add basic title and description for front page

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,9 @@
 require('dotenv').config({ path: '.env' })
 
 const DEFAULT_ENVS = {
-  BASE_URL: "https://rlnc.ru",
+  BASE_URL: "http://localhost:8080",
+  BASE_TITLE: "РЛНС",
+  BASE_DESCRIPTION: "Международная научно-техническая конференция «Радиолокация, навигация, связь»",
   CONFERENCES: "2017, 2018, 2020, 2021, 2022, 2023, 2024, 2025",
   SERVER_PATH: "/web/sites/rlnc/www/",
   GITLAB_TOKEN: "",
@@ -13,6 +15,8 @@ function getEnv(envKey) {
 
 module.exports = {
   baseUrl: getEnv('BASE_URL'),
+  baseTitle: getEnv('BASE_TITLE'),
+  baseDescription: getEnv('BASE_DESCRIPTION'),
   conferences: getEnv('CONFERENCES'),
   serverPath: getEnv('SERVER_PATH'),
   gitlabToken: getEnv('GITLAB_TOKEN'),
