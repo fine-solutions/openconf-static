46f751a35954bd0ec9b50b055bd3cb1612c6b7b2
Igor Korovchenko <igsekor@gmail.com>
2025-02-07 06:32:16 +0300

    Remove all empty sections' data

--- src/views/index.11tydata.js
+++ src/views/index.11tydata.js
@@ -1,3 +1,48 @@
+function getNumberWords(value, words) {
+  value = Math.abs(value) % 100;
+	var num = value % 10;
+	if(value > 10 && value < 20) return words[2];
+	if(num > 1 && num < 5) return words[1];
+	if(num == 1) return words[0];
+	return words[2];
+}
+
+function getUpdatedTime(time) {
+  const currentTime = new Date()
+  const updatedTime = new Date(time)
+  const diff = Math.abs((updatedTime - currentTime) / 1000)
+  let value = 0, word = ''
+  if (diff < 60) {
+    value =   Math.floor(diff)
+    word = getNumberWords(value, ['секунда', 'секунды', 'секунд'])
+    return `${value} ${word} назад`
+  } else if (diff < 60*60) {
+    value = Math.ceil(diff / 60)
+    word = getNumberWords(value, ['минута', 'минуты', 'минут'])
+    return `${value} ${word} назад`
+  } else if (diff < 60*60*24) {
+    value = Math.ceil(diff / (60*60))
+    word = getNumberWords(value, ['час', 'часа', 'часов'])
+    return `${value} ${word} назад`
+  } else if (diff < 60*60*24*7) {
+    value = Math.ceil(diff / (60*60*24))
+    word = getNumberWords(value, ['день', 'дня', 'дней'])
+    return `${value} ${word} назад`
+  } else if (diff < 60*60*24*7*30) {
+    value = Math.ceil(diff / (60*60*24*7))
+    word = getNumberWords(value, ['неделя', 'недели', 'недель'])
+    return `${value} ${word} назад`
+  } else if (diff < 60*60*24*7*30*12) {
+    value = Math.ceil(diff / (60*60*24*7*30))
+    word = getNumberWords(value, ['месяц', 'месяца', 'месяцев'])
+    return `${value} ${word} назад`
+  } else if (diff > 60*60*24*7*365) {
+    value = Math.ceil(diff / (60*60*24*7*365))
+    word = getNumberWords(value, ['год', 'года', 'лет'])
+    return `${value} ${word} назад`
+  }
+}
+
 export default {
   layout: 'base.njk',
   permalink: '/',
@@ -35,65 +80,26 @@ export default {
     },
 
     bannersItems: function () {
-      return [
-        {
-          label: 'Некоторые значимые достижения российских ученых в области информационных технологий',
-          url: '/featured/achievements',
-          title: 'Описание',
-          src: '/images/index/lights',
-          alt: 'Описание',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary'
-            },
-          ],
-        },
-        {
-          label: 'Основные направления устойчивого развития предприятий радиоэлектронного комплекса в 2020–2030 годы',
-          url: '/featured/ways',
-          title: 'Описание',
-          src: '/images/index/antenna',
-          alt: 'Описание',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary'
-            },
-          ],
-        },
-        {
-          label: 'Фракталы, метаповерхности, системы РСА и MIMO: перспективы развития и стратегия технологий',
-          url: '/featured/fractals',
-          title: 'Описание',
-          src: '/images/index/satellite',
-          alt: 'Описание',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary'
-            },
-          ],
-        }
-      ]
+      return undefined
     },
 
     calendarItems: function () {
+      const currentDate = new Date()
       return [
         {
-          title: '8 февраля — 20 марта',
-          text: 'Прием докладов на сайте конференции',
-          isActive: true
+          title: '10 февраля — 20 марта',
+          text: 'Прием и рецензирование докладов',
+          isActive: currentDate >= new Date('2025-02-10T00:00+02:00') && currentDate <= new Date('2025-03-20T00:00+02:00'),
         },
         {
           title: '1 апреля — 10 апреля',
-          text: 'Регистрация участников на сайте конференции',
-          isActive: false
+          text: 'Регистрация участников',
+          isActive: currentDate >= new Date('2025-04-01T00:00+02:00') && currentDate <= new Date('2025-04-10T00:00+02:00'),
         },
         {
           title: '15 апреля — 17 апреля',
           text: 'Работа секций конференции',
-          isActive: false
+          isActive: currentDate >= new Date('2025-04-15T00:00+02:00') && currentDate <= new Date('2025-04-17T00:00+02:00'),
         }
       ]
     },
@@ -105,14 +111,14 @@ export default {
           type: 'link',
           url: '',
           title: 'Состав оргкомитета',
-          isActive: true,
+          isActive: false,
         },
         {
           label: 'Информационное письмо',
           type: 'pdf',
-          url: '',
+          url: '/docs/rlnc-call-for-paper.pdf',
           title: 'Информационное письмо в формате PDF',
-          isActive: true,
+          isActive: false,
         },
         {
           label: 'Шаблоны оформления',
@@ -148,67 +154,16 @@ export default {
     newsItems: function () {
       return [
         {
-          text: 'Открыта регистрация участников очных и онлайн секций XXX международной научно-технической конференции «Радиолокация, навигация, связь».',
-          updated: '2025-02-03T09:20+02:00',
-          time: '6 часов назад',
-          tags: [
-            {
-              label: 'Регистрация',
-              url: '/news/tags/registration',
-            },
-            {
-              label: 'Важно',
-              url: '/news/tags/important',
-            }
-          ],
-          url: '/news',
-          title: 'Вы можете перейти к новостям'
-        },
-        {
-          text: 'Приём докладов продлится до 24 марта (воскресенье).',
-          updated: '2025-02-02T09:20+02:00',
-          time: '1 день назад',
-          tags: [
-            {
-              label: 'Сроки',
-              url: '/news/tags/dates',
-            },
-            {
-              label: 'Важно',
-              url: '/news/tags/important',
-            }
-          ],
-          url: '/news',
-          title: 'Вы можете перейти к новостям'
-        },
-        {
-          text: 'Приём докладов на XXXI международную научно-техническую конференцию «Радиолокация, навигация, связь» (RLNC*2025) начнется 8 февраля.',
-          updated: '2025-02-01T09:20+02:00',
-          time: '2 дня назад',
-          tags: [
-            {
-              label: 'Сроки',
-              url: '/news/tags/dates',
-            },
-            {
-              label: 'Важно',
-              url: '/news/tags/important',
-            }
-          ],
-          url: '/news',
-          title: 'Вы можете перейти к новостям'
-        },
-        {
           text: 'Мы запустили наш новый сайт и систему подачи докладов',
           updated: '2025-01-01T09:20+02:00',
-          time: '3 дня назад',
+          time: getUpdatedTime('2025-02-08T09:20+02:00'),
           tags: [
             {
               label: 'Важно',
-              url: '/news/tags/important',
+              url: '/#news',
             }
           ],
-          url: '/news',
+          url: '/#news',
           title: 'Вы можете перейти к новостям'
         },
       ]
@@ -272,44 +227,7 @@ export default {
     },
 
     speakerItems: function () {
-      return [
-        {
-          name: 'Иванов Петр Сидорович',
-          url: '/',
-          title: 'Почитать об Иванове Петре Сидоровиче',
-          image: '/images/speakers/ivanov',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary',
-            }
-          ],
-        },
-        {
-          name: 'Петров Сидор Иванович',
-          url: '/',
-          title: 'Почитать о Петрове Сидоре Ивановиче',
-          image: '/images/speakers/ivanov',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary',
-            }
-          ],
-        },
-        {
-          name: 'Сидоров Иван Петрович',
-          url: '/',
-          title: 'Почитать о Сидорове Иване Петровиче',
-          image: '/images/speakers/ivanov',
-          tags: [
-            {
-              label: 'Пленарный доклад',
-              url: '/section/plenary',
-            }
-          ],
-        }
-      ]
+      return undefined
     },
 
     partnerItems: function () {
@@ -337,14 +255,14 @@ export default {
         {
           label: 'Benefit Plaza',
           text: 'Скидка на все категории номеров 15%. В стоимость включены: завтрак «шведский стол», Интернет, пользование тренажерным залом. К услугам гостей 173 просторных комфортабельных номера. Отель гарантирует безопасность и комфортабельный',
-          url: '/accommodation/benefit-plaza',
+          url: 'https://www.benefitplaza.ru/contact.htm',
           title: 'Перейти на страницу с описанием условий размещения в отеле Benefit Plaza',
           price: 3750,
         },
         {
           label: 'Degas',
           text: 'Завтрак «Шведский стол» включен в стоимость проживания. Стоимость дополнительного завтрака 700 руб/чел.',
-          url: '/accommodation/degas',
+          url: 'https://degas-hotel.ru',
           title: 'Перейти на страницу с описанием условий размещения в отеле Degas',
           price: 4500,
         },
@@ -352,14 +270,15 @@ export default {
     },
 
     archiveItems: function (data) {
-      return data.constants.archiveYears
-        .map((y, i) => {
-          return {
-            url: `/archive/${y}`,
-            label: data.constants.currentNumber - data.constants.archiveYears.length + i,
-          }
-        })
-        .sort((a, b) => b.label - a.label)
+      return undefined
+      // return data.constants.archiveYears
+      //   .map((y, i) => {
+      //     return {
+      //       url: `/archive/${y}`,
+      //       label: data.constants.currentNumber - data.constants.archiveYears.length + i,
+      //     }
+      //   })
+      //   .sort((a, b) => b.label - a.label)
     }
   },
 }
