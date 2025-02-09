21d0662d2a0f4feb4937eae6eef2cf28899953b0
Igor Korovchenko <igsekor@gmail.com>
2025-02-09 23:07:38 +0300

    Add news-list

---
+++ src/views/news-list.11tydata.js
@@ -0,0 +1,26 @@
+export default {
+  layout: 'base.njk',
+  permalink: '/news/index.html',
+  fullPageUrl: '/news',
+  documentTitle: `RLNC*2025 | Новости`,
+  documentDescription: `Новости конференции RLNC*2025`,
+
+  eleventyComputed: {
+    breadcrumbs: function () {
+      return [
+        {
+          url: '/',
+          label: 'Главная'
+        },
+        {
+          url: '/news',
+          label: 'Новости'
+        },
+      ]
+    },
+
+    newsList: function (data) {
+      return data.collections.news
+    },
+  },
+}
