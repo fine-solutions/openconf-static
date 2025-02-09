d294764c482431fbef391bc605c0f7823fd901ae
Igor Korovchenko <igsekor@gmail.com>
2025-02-09 03:04:37 +0300

    Fix script issue

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-import { getNumberWords } from "./temporal/index.js"
+import { getUpdatedTime } from './time-pass/index.js'
 
 const timeBlocks = document.getElementsByTagName('time')
 if (timeBlocks.length > 0) {
   for (let i = 0; i < timeBlocks.length; i++) {
-    timeBlocks[i].innerText = getNumberWords(timeBlocks[i].innerText)
+    timeBlocks[i].innerText = getUpdatedTime(timeBlocks[i].getAttribute('datetime'))
   }
 }
