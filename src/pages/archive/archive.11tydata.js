956f03e8eedae3d4e465c70a39bae3f22f9ccd1c
Denis Mukonin <dezmukonin16@gmail.com>
2025-02-22 00:10:28 +0300

    Fix data for archive page

--- src/pages/archive/archive.11tydata.js
+++ src/pages/archive/archive.11tydata.js
@@ -40,10 +40,66 @@ export default {
       // return documents.filter((t) => data.page.fileSlug == t.url)
       return [
         {
+          label: 'Оргкоммитет',
+          type: 'pdf',
+          url: '',
+          title: 'Оргкоммитет конференции',
+          isActive: true,
+        },
+        {
           label: 'Информационное письмо',
           type: 'pdf',
           url: '/docs/2025/rlnc-call-for-paper.pdf',
           title: 'Информационное письмо в формате PDF',
+          isActive: true,
+        },
+        {
+          label: 'Программа',
+          type: 'pdf',
+          url: '',
+          title: 'Программа конференции',
+          isActive: true,
+        },
+        {
+          label: 'Сборник трудов (том 1)',
+          type: 'pdf',
+          url: '',
+          title: 'Сборник трудов (том 1) конференции',
+          isActive: true,
+        },
+        {
+          label: 'Сборник трудов (том 2)',
+          type: 'pdf',
+          url: '',
+          title: 'Сборник трудов (том 2) конференции',
+          isActive: true,
+        },
+        {
+          label: 'Сборник трудов (том 3)',
+          type: 'pdf',
+          url: '',
+          title: 'Сборник трудов (том 3) конференции',
+          isActive: true,
+        },
+        {
+          label: 'Сборник трудов (том 4)',
+          type: 'pdf',
+          url: '',
+          title: 'Сборник трудов (том 4) конференции',
+          isActive: true,
+        },
+        {
+          label: 'Сборник трудов (том 5)',
+          type: 'pdf',
+          url: '',
+          title: 'Сборник трудов (том 5) конференции',
+          isActive: true,
+        },
+        {
+          label: 'Лучшие доклады на секциях',
+          type: 'pdf',
+          url: '',
+          title: 'Лучшие доклады на секциях конференции',
           isActive: true,
         },
         {
