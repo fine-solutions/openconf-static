2b8b04c883467368fadc6a4884ddc806e4f97ed2
Igor Korovchenko <igsekor@gmail.com>
2025-03-01 20:04:57 +0300

    Fix archive docs for the page

--- src/pages/archive/archive.11tydata.js
+++ src/pages/archive/archive.11tydata.js
@@ -35,85 +35,15 @@ export default {
       ]
     },
 
-    documentsList: function (data) {
-      // const { documents } = data.collections
-      // return documents.filter((t) => data.page.fileSlug == t.url)
-      return [
-        {
-          label: 'Оргкоммитет',
-          type: 'pdf',
-          url: '',
-          title: 'Оргкоммитет конференции',
-          isActive: true,
-        },
-        {
-          label: 'Информационное письмо',
-          type: 'pdf',
-          url: '/docs/2025/rlnc-call-for-paper.pdf',
-          title: 'Информационное письмо в формате PDF',
-          isActive: true,
-        },
-        {
-          label: 'Программа',
-          type: 'pdf',
-          url: '',
-          title: 'Программа конференции',
-          isActive: true,
-        },
-        {
-          label: 'Сборник трудов (том 1)',
-          type: 'pdf',
-          url: '',
-          title: 'Сборник трудов (том 1) конференции',
-          isActive: true,
-        },
-        {
-          label: 'Сборник трудов (том 2)',
-          type: 'pdf',
-          url: '',
-          title: 'Сборник трудов (том 2) конференции',
-          isActive: true,
-        },
-        {
-          label: 'Сборник трудов (том 3)',
-          type: 'pdf',
-          url: '',
-          title: 'Сборник трудов (том 3) конференции',
-          isActive: true,
-        },
-        {
-          label: 'Сборник трудов (том 4)',
-          type: 'pdf',
-          url: '',
-          title: 'Сборник трудов (том 4) конференции',
-          isActive: true,
-        },
-        {
-          label: 'Сборник трудов (том 5)',
-          type: 'pdf',
-          url: '',
-          title: 'Сборник трудов (том 5) конференции',
-          isActive: true,
-        },
-        {
-          label: 'Лучшие доклады на секциях',
-          type: 'pdf',
-          url: '',
-          title: 'Лучшие доклады на секциях конференции',
-          isActive: true,
-        },
-        {
-          label: 'Фотографии',
-          type: 'zip',
-          url: '',
-          title: 'Фотографии конференции',
-          isActive: true,
-        },
-      ]
+    fileList: function (data) {
+      const { documents } = data
+      return documents?.map((d) => {
+        return {
+          label: d.label,
+          url: `/docs/${data.page.fileSlug}/${d.url}`,
+          type: d.url.split('.')[1]
+        }
+      })
     },
-
-    images: function () {
-      return undefined
-    }
   },
 }
